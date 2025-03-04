7c38cfd6e169a33f41d732f668fd768b02f9bd2a
Gaurav Shukla <yoursgaurav@yahoo.com>
2025-03-04 19:42:42 +0530

    Add NotFound component for handling 404 errors with user-friendly message

---
+++ app/not-found.tsx
@@ -0,0 +1,25 @@
+// External dependencies
+import Link from "next/link";
+
+// Local components
+import TypographyH1 from "@/components/typography/typography-h1";
+import Wrapper from "@/components/layout/wrapper";
+
+export default function NotFound() {
+  return (
+    <section className="min-h-[85vh] grid content-center">
+      <Wrapper className="grid gap-6 text-center md:gap-8 lg:gap-10">
+        <TypographyH1>404 - Page Not Found</TypographyH1>
+        <p className="text-muted-foreground text-lg">
+          Oops! It looks like this page doesn’t exist.
+        </p>
+        <Link
+          href="/"
+          className="text-primary hover:underline text-lg font-medium"
+        >
+          Return Home
+        </Link>
+      </Wrapper>
+    </section>
+  );
+}
