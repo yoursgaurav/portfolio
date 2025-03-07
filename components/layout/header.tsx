70910e64cbc0d930d069f62152640af657faa8a7
Gaurav Shukla <yoursgaurav@yahoo.com>
2025-03-07 12:46:19 +0530

    Revamp Header component to use Sheet for mobile navigation

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-// External dependencies (React, Next.js, and third-party libraries)
-import { useState } from "react";
+// External dependencies
 import Link from "next/link";
 import { useTheme } from "next-themes";
-import { Moon, Sun, Menu, X } from "lucide-react";
+import { ChevronDown, Moon, Sun } from "lucide-react";
 
-// Local UI components (Shadcn/UI)
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+// Local components
+import Wrapper from "./wrapper";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -15,105 +14,102 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
-// Local utilities and layout components
-import { cn } from "@/lib/utils";
-import Wrapper from "./wrapper";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 // Constants and data
 import { websiteLinks } from "@/constants/navigation";
 
-const AVATAR_CONFIG = {
-  src: "/me.jpg",
-  alt: "Gaurav Shukla's Profile Picture",
-  fallback: "GS",
-};
-
 export default function Header() {
   const { setTheme } = useTheme();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  // Toggle mobile navigation menu
-  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
 
   return (
-    <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 @container sticky top-0 z-50 w-full border-b backdrop-blur">
-      <Wrapper className="flex items-center justify-between py-3 @5xl:py-4">
-        {/* User logo */}
-        <Link href="/" aria-label="Navigate to homepage">
-          <Avatar className="size-10 @3xl:size-11">
-            <AvatarImage src={AVATAR_CONFIG.src} alt={AVATAR_CONFIG.alt} />
-            <AvatarFallback>{AVATAR_CONFIG.fallback}</AvatarFallback>
-          </Avatar>
-        </Link>
+    <header className="sticky top-0 z-50">
+      <Wrapper className="flex items-center justify-end gap-3 py-7 md:justify-between">
+        {/* Empty div for layout purposes */}
+        <div aria-hidden="true" />
 
-        {/* Navigation and Controls */}
-        <div className="flex items-center gap-2 @3xl:gap-8 @5xl:gap-10 @7xl:gap-12">
-          {/* Navigation */}
-          <nav
-            id="header-nav"
-            className={cn(
-              "@max-3xl:bg-background @max-3xl:absolute @max-3xl:inset-x-0 @max-3xl:top-full @max-3xl:border-y",
-              { "@max-3xl:hidden": !isMenuOpen }
-            )}
-          >
-            <ul className="grid @3xl:flex @3xl:items-center @3xl:gap-8 @5xl:gap-10 @7xl:gap-12">
-              {websiteLinks.map((link) => (
-                <li
-                  key={link.name}
-                  className="@max-3xl:px-6 @max-3xl:py-4 @max-3xl:text-right @max-3xl:not-first:border-t"
-                >
-                  <Link
-                    href={link.url}
-                    className="text-muted-foreground hover:text-primary focus-visible:outline-primary text-sm font-bold tracking-widest uppercase focus-visible:outline-2 focus-visible:outline-offset-2 transition-all @3xl:@max-5xl:text-xs"
-                  >
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-
-          {/* Theme Switcher */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild className="cursor-pointer">
-              <Button variant="outline" size="icon">
-                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                <span className="sr-only">Switch theme</span>
+        {/* Navigation Section */}
+        <nav>
+          {/* Mobile Navigation Menu */}
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="secondary"
+                size="lg"
+                className="border md:hidden"
+              >
+                Menu <ChevronDown className="opacity-50" />
               </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => setTheme("light")}>
-                Light
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("dark")}>
-                Dark
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("system")}>
-                System
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+            </SheetTrigger>
+            <SheetContent side="bottom">
+              <SheetHeader className="sr-only">
+                <SheetTitle>Navigation Menu</SheetTitle>
+                <SheetDescription>
+                  Browse website navigation links
+                </SheetDescription>
+              </SheetHeader>
+              <ul className="grid gap-3 px-5 py-11">
+                {websiteLinks.map((link) => (
+                  <li key={link.name}>
+                    <SheetClose asChild>
+                      <Link href={link.url} className="text-lg font-medium">
+                        {link.name}
+                      </Link>
+                    </SheetClose>
+                  </li>
+                ))}
+              </ul>
+            </SheetContent>
+          </Sheet>
 
-          {/* Mobile Navigation Toggler */}
-          <Button
-            variant="outline"
-            size="icon"
-            className="@3xl:hidden"
-            onClick={toggleMenu}
-            aria-label={
-              isMenuOpen ? "Close navigation menu" : "Open navigation menu"
-            }
-            aria-expanded={isMenuOpen}
-            aria-controls="header-nav"
-          >
-            {isMenuOpen ? <X /> : <Menu />}
-            <span className="sr-only">
-              {isMenuOpen ? "Close menu" : "Open menu"}
-            </span>
-          </Button>
-        </div>
+          {/* Desktop Navigation Menu */}
+          <ul className="bg-secondary hidden items-center gap-7 rounded-md border px-6 py-2 md:flex">
+            {websiteLinks.map((link) => (
+              <li key={link.name}>
+                <Link
+                  href={link.url}
+                  className="hover:text-primary text-sm font-medium transition-colors"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        {/* Theme Switcher Dropdown */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="secondary"
+              size="lg"
+              className="cursor-pointer border"
+            >
+              <Sun className="size-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute size-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Switch theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </Wrapper>
     </header>
   );
