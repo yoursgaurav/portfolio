77491efe475e8df46992086fe53f831ac35b75df
Gaurav Shukla <yoursgaurav@yahoo.com>
2025-03-17 15:01:57 +0530

    Revamp NotFound component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,24 @@
 // External dependencies
 import Link from "next/link";
+import { Rabbit } from "lucide-react";
 
 // Local components
 import Wrapper from "@/components/layout/wrapper";
 import TypographyH1 from "@/components/typography/typography-h1";
+import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
   return (
-    <section className="min-h-screen content-center">
-      <Wrapper className="mx-auto max-w-2xl space-y-6 text-center">
-        <TypographyH1>404 - Page Not Found</TypographyH1>
-        <p className="text-muted-foreground">
-          Oops! It looks like this page doesn’t exist.
+    <section className="mt-32 content-center">
+      <Wrapper className="mx-auto grid max-w-2xl justify-items-center gap-4 text-center md:gap-6">
+        <Rabbit className="size-24 stroke-1 lg:size-36" aria-hidden="true" />
+        <TypographyH1>Page not found</TypographyH1>
+        <p className="text-muted-foreground text-pretty">
+          Sorry, we couldn’t find the page you’re looking for.
         </p>
-        <Link href="/" className="text-primary font-medium hover:underline">
-          Return Home
-        </Link>
+        <Button asChild variant="outline">
+          <Link href="/">Go back home</Link>
+        </Button>
       </Wrapper>
     </section>
   );
